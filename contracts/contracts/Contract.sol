f11d240e3c513fb599044eba27182b0eb9829208
Hami Keskin <hami.keskin@bil.omu.edu.tr>
2023-07-13 11:36:10 +0300

    Update Contract.sol

--- contracts/contracts/Contract.sol
+++ contracts/contracts/Contract.sol
@@ -2,33 +2,40 @@
 pragma solidity ^0.8.0;
 
 contract RockPaperScissors {
-    enum Move { None, Rock, Paper, Scissors }
-    uint8[3][3] payoffMatrix = [[0,2,1],[1,0,2],[2,1,0]];
-    
+    enum Move {
+        None,
+        Rock,
+        Paper,
+        Scissors
+    }
+    uint8[3][3] private payoffMatrix = [[0, 2, 1], [1, 0, 2], [2, 1, 0]];
     struct Player {
         address addr;
         Move move;
         bool played;
     }
-    
+
     Player public player;
-    
+
     function play(Move _move) public {
         require(_move >= Move.Rock && _move <= Move.Scissors, "Invalid move.");
-        require(player.addr == address(0) || player.addr == msg.sender, "Not the player.");
+        require(
+            player.addr == address(0) || player.addr == msg.sender,
+            "Not the player."
+        );
         require(!player.played, "Already played.");
-        
+
         player.addr = msg.sender;
         player.move = _move;
         player.played = true;
     }
-    
+
     function determineResult() public view returns (string memory) {
         require(player.played, "Player has not played yet.");
-        
-        uint randomMove = getRandomMove();
-        uint8 result = payoffMatrix[uint8(player.move)][randomMove];
-        
+
+        Move randomMove = getRandomMove();
+        uint8 result = payoffMatrix[uint8(player.move)][uint8(randomMove)];
+
         if (result == 1) {
             return "Player wins!";
         } else if (result == 2) {
@@ -37,17 +44,19 @@ contract RockPaperScissors {
             return "It's a draw!";
         }
     }
-    
+
     function resetGame() public {
         require(player.played, "Player has not played yet.");
-        
+
         player.addr = address(0);
         player.move = Move.None;
         player.played = false;
     }
-    
-    function getRandomMove() internal view returns (uint) {
-        uint randomNumber = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender))) % 3;
-        return randomNumber;
+
+    function getRandomMove() private view returns (Move) {
+        uint256 randomNumber = uint256(
+            keccak256(abi.encodePacked(block.timestamp, msg.sender))
+        ) % 3;
+        return Move(randomNumber);
     }
 }
