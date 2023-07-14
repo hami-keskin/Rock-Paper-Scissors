9082eac6eb29c31918bf510220b96794ed031e11
Hami Keskin <hami.keskin@bil.omu.edu.tr>
2023-07-14 11:58:08 +0300

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,20 +1,10 @@
-import { Sepolia } from "@thirdweb-dev/chains";
 import {
-  ThirdwebProvider,
   useContract,
   useContractRead,
   useContractWrite,
 } from "@thirdweb-dev/react";
 
 function App() {
-  return (
-    <ThirdwebProvider activeChain={Sepolia}>
-      <Component />
-    </ThirdwebProvider>
-  );
-}
-
-function Component() {
   const { contract, isLoading } = useContract(
     "0x3D01d7C35143f47bdDDaC0324a38A532e88E1B6a"
   );
